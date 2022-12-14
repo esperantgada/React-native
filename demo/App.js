3a45e8a005d1266a3a3997b12dc6f82c9a9507db
Espérant GADA <esperantgada@gmail.com>
2022-12-14 02:41:10 +0100

    Use SectionList in react native

--- demo/App.js
+++ demo/App.js
@@ -6,83 +6,142 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
+ import React, {useState} from 'react';
 
-import {
-  Button,
-  Linking,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ import {
+   FlatList,
+   RefreshControl,
+   ScrollView,
+   SectionList,
+   StyleSheet,
+   Text,
+   View
+ } from 'react-native';
+ 
+ 
+ /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+  * LTI update could not be added via codemod */
+ 
+ 
+ function App () {
+ 
+   const [refresh, setRefresh] = useState(false)
+ 
+ 
+   //The three dot here mean "the following items"
+   //When page refreshes, it will add these three items
+ 
+   const onRefreshView = () => {
+     setRefresh(true)
+     setItem([...items, {item: 'item 19'}, {item: 'item 20'}, {item: 'item 21'}])
+     setRefresh(false)
+   }
+ 
+ 
+   //A list of item predefined using useSate
+   const [items, setItem] = useState([
+     {item: 'item 1'},
+     {item: 'item 2'},
+     {item: 'item 3'},
+     {item: 'item 4'},
+     {item: 'item 5'},
+     {item: 'item 6'},
+     {item: 'item 7'},
+     {item: 'item 8'},
+     {item: 'item 9'},
+     {item: 'item 10'},
+     {item: 'item 11'},
+     {item: 'item 12'},
+     {item: 'item 13'},
+     {item: 'item 14'},
+     {item: 'item 15'},
+     {item: 'item 16'},
+     {item: 'item 17'},
+     {item: 'item 18'}
+     
+   ]);
 
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
+   const DATA = [
+      {
+        title : 'Title 1',
+        data : ['Item 1-1', 'Item 1-2', 'Item 1-3', 'Item 1-4'],
+      },
 
-function countInitial(){
-  console.log('Run function')
-  return 0
-}
+      {
+        title : 'Title 2',
+        data : ['Item 2-1', 'Item 2-2', 'Item 2-3', 'Item 2-4'],
+      },
 
-function App () {
+      {
+        title : 'Title 3',
+        data : ['Item 3-1', 'Item 3-2', 'Item 3-3', 'Item 3-4'],
+      },
 
-  const [count, setCount] = useState(() => countInitial())
-  const [session, setSession]= useState({number: 97756877, title: 'React Native'})
-  const [name, setName] = useState('Esperant')
+      {
+        title : 'Title 4',
+        data : ['Item 4-1', 'Item 4-2', 'Item 4-3', 'Item 4-4'],
+      },
 
-  function decrementCount(){
-    setCount(preViewCount => preViewCount - 1)
-  }
+      {
+        title : 'Title 5',
+        data : ['Item 5-1', 'Item 5-2', 'Item 5-3', 'Item 5-4'],
+      },
+   ]
+ 
+   return (
 
-  function incrementCount(){
-    setCount(preViewCount => preViewCount + 1)
-  }
+    <SectionList
+        keyExtractor = {(item, index) => index.toString()}
 
-  function handleUpdate(){
-    setName('Esperant GADA')
-    setSession({number: 45, title: 'Programmer'})
-  }
+        sections = {DATA}
+        renderItem = {({item}) => (
+           <Text style = {styles.text}> {item}</Text>
+      
+        )}
 
-  return (
+        renderSectionHeader = {({section}) =>(
+          <View style = {styles.item}>
+              <Text style = {styles.text}> {section.title}</Text>
+          </View>
+        )}
 
-   <View style = {styles.body}>
-
-      <Button style={styles.buttons} title='-' onPress={decrementCount}></Button>
-      <Text style={styles.text}>{count}</Text>
-      <Button title='+' onPress={incrementCount} style={styles.buttons}></Button>
-
-      <Text style={styles.text}>You clicked {count} times the button.</Text>
-
-
-      <Text style={styles.text}> {name} is a  {session.title} developer. His number is {session.number}</Text>
-      <Button title='Click me' onPress={()=> {Linking.openURL('https://reactnative.dev/')}}></Button>
-      <Button title='Update value' onPress={handleUpdate} style= {styles.buttons}></Button>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  
-  body:{
-      alignItems:'center',
-      flex:1,
-      justifyContent:'center'
-  },
-
-  text:{
-
-    fontSize: 24,
-    fontStyle:'italic',
-    marginLeft: 20,
-    marginEnd: 20
-  },
-
-  buttons:{
-    width: 100,
-    height: 100,
-    marginTop: 50
-  },
-});
-
-export default App;
+        refreshControl = {
+        <RefreshControl refreshing = {refresh} 
+            onRefresh = {onRefreshView} 
+            colors = {['#ff00ff']}
+        />}
+    
+    />
+ 
+   );
+ }
+ 
+ const styles = StyleSheet.create({
+   
+   body:{
+       flex:1,
+       backgroundColor: '#ffffff',
+       flexDirection: 'column',
+   },
+ 
+   text:{
+     color: '#000000',
+     fontSize: 40,
+     fontStyle:'italic',
+     margin: 10
+   },
+ 
+ 
+   item:{
+     backgroundColor: '#4ae1fa',
+     borderRadius: 10,
+     justifyContent: 'center',
+     alignItems: 'center',
+     margin: 10
+   }
+ 
+ });
+ 
+ export default App;
+ 
